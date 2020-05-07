7c473096c982404603703265dd350fabdbeefe32
marco <marco@LAPTOP-HITOHP1E>
2020-05-07 00:14:07 -0300

    Configuração para migrar a aplicação para Spring Boot 2 concluída.

--- src/main/java/br/agenda3/Application.java
+++ src/main/java/br/agenda3/Application.java
@@ -2,11 +2,18 @@ package br.agenda3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "br.agenda3" })
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public CacheManager cacheManager() {
+	    return new org.springframework.cache.concurrent.ConcurrentMapCacheManager();
 	}
 }
